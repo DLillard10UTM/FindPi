2dfbb3ea046f0d8c030a3520dc92f8977bbc93c2
Danny Lillard <danllill@ut.utm.edu>
2020-03-10 16:14:31 -0500

    Added vars and constructor to FindPiThread.

---
+++ FindPi/FindPiThread.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindPi
+{
+    class FindPiThread
+    {
+        //An int holding the number of darts it needs to throw
+        int numThrown;
+        //An int holding the count of darts that lands inside your dartboard(quarter)
+        public int numSuccess { get; set; }
+        //A Random - this will be used to generate throws!
+        Random rnd;
+
+        FindPiThread(int numToThrow)
+        {
+            rnd = new Random();
+            numThrown = 0;
+            numSuccess = 0;
+        }
+    }
+}
