f70a3807248d142785ee1f23fd33bc4b15f420d0
Danny Lillard <danllill@ut.utm.edu>
2020-03-11 15:08:54 -0500

    Completed the problem.

--- FindPi/FindPiThread.cs
+++ FindPi/FindPiThread.cs
@@ -2,22 +2,55 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*
+ * Author: Danny Lillard
+ * Date: 3/11/2020
+ * Description: This class contains the logic to find pi using the 
+ *              Monte Carlo method.
+ */
 namespace FindPi
 {
     class FindPiThread
     {
         //An int holding the number of darts it needs to throw
-        int numThrown;
+        int numToThrow;
         //An int holding the count of darts that lands inside your dartboard(quarter)
         public int numSuccess { get; set; }
         //A Random - this will be used to generate throws!
         Random rnd;
 
-        FindPiThread(int numToThrow)
+        /*
+         * Paramertized constructor, sets numToThrow to userInput to control loops 
+         * later in the class.
+         */
+        public FindPiThread(int userInput)
         {
             rnd = new Random();
-            numThrown = 0;
+            numToThrow = userInput;
             numSuccess = 0;
+        }
+
+        /*
+         * This function is used to calculate pi. It randomly selects two doubles from
+         * 0 to 1 and finds the hypotenuse between them, if the hypotenuse is equal to,
+         * or 1 then the point falls in the circle.
+         */
+        public void throwDarts()
+        {
+            double x;
+            double y;
+            double hypo;
+            for(int i = 0; i < numToThrow; i++)
+            {
+                x = rnd.NextDouble();
+                y = rnd.NextDouble();
+
+                hypo = Math.Sqrt(x * x + y * y);
+                if(hypo <= 1)
+                {
+                    numSuccess++;
+                }
+            }
         }
     }
 }
--- FindPi/Program.cs
+++ FindPi/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
+/*
+ * Author: Danny Lillard
+ * Date: 3/11/2020
+ * Description: This class runs the FindPiThread.cs class. It is responsible for
+ *              asking the user how many times to run the simulation, threading instances
+ *              of the class, and running those threads. It then calculates the pi value
+ *              for the experiment.
+ */
 namespace FindPi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //This number is all threads successful darts added together.
+            int totalNumSuccess = 0;
+            Console.WriteLine("Please enter how many darts you would like to throw.");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Please enter how many threads you would like to run.");
+            string threadNumInput = Console.ReadLine();
+
+            List<Thread> threads =  new List<Thread>();
+            List<FindPiThread> piThreads = new List<FindPiThread>();
+
+
+            for (int i = 0; i < Convert.ToInt32(threadNumInput); i++)
+            {
+                piThreads.Add(new FindPiThread(Int32.Parse(userInput)));
+
+                threads.Add(new Thread(new ThreadStart(piThreads[i].throwDarts)));
+
+                threads[i].Start();
+
+                Thread.Sleep(16);
+            }
+            for(int i = 0; i < Convert.ToInt32(threadNumInput); i++)
+            {
+                threads[i].Join();
+            }
+            for(int i = 0; i < Convert.ToInt32(threadNumInput); i++)
+            {
+                totalNumSuccess = piThreads[i].numSuccess;
+            }
+            Console.WriteLine(4.0 * (totalNumSuccess) / (Convert.ToInt32(userInput)));
+            Console.ReadKey();
         }
     }
 }
